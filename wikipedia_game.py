fcf23c49845466a5aeeef1c487f2cf46accdeea5
Denim - Jed Meier <jmeier@denimlabs.com>
2019-08-29 17:26:09 -0500

    added pooling?

--- wikipedia_game.py
+++ wikipedia_game.py
@@ -1,8 +1,13 @@
-import wikipedia
+# import wikipedia
 import re
 import sys
 
-wikipedia.set_rate_limiting(True)
+import wikipediaapi
+wiki = wikipediaapi.Wikipedia('en')
+
+from multiprocessing import Pool
+# wikipedia.set_rate_limiting(True)
+from collections import deque
 
 
 def remove_bad_links(links):
@@ -19,17 +24,31 @@ def remove_bad_links(links):
     return cleaned.split(',')
 
 
-def get_links(name):
+# def get_links(name):
+#     try:
+#         page = wikipedia.page(name.title())
+#         return remove_bad_links(page.links)
+#     except Exception:
+#         return []
+
+def get_links(id):
+    # print("getting links for: ",name)
+
     try:
-        page = wikipedia.page(name.title())
-        return remove_bad_links(page.links)
-    except Exception:
+        page = wiki.page(name)
+        return remove_bad_links(wiki.page(name).links)
+    except Exception as e:
         return []
 
+def get_id(name):
+  try:
+    return wiki.page(name).pageid
+  except Exception:
+    return []
 
 def process_node(node, queue, end, path):
     links = get_links(node)
-
+    # print("processing: ",)
     for link in links:
         new_path = list(path)
         new_path.append(link)
@@ -44,52 +63,60 @@ def process_node(node, queue, end, path):
 def format_return(path, depth, is_found=False):
     return {
             'path': path,
-            'pretty_path': ' -> '.join(path).title(),
+            # 'pretty_path': ' -> '.join(path).title(),
             'depth': depth,
             'is_found': str(is_found)
             }
 
 
 def fn_queue(queue, visited, end, show_updates, depth=1):
-    path = queue.pop(0)
-    node = path[-1]
+    if queue:
+        # print("queue len: ",len(queue))
 
-    if show_updates:
-        if len(path) > depth:
-            formatted = format_return(path, depth)
-            print(formatted)
+        # path = queue.popleft()
+        path = queue.pop(0)
+        node = path[-1]
 
-    depth = len(path)
-    if node not in visited:
-        result = process_node(node, queue, end, path)
-        if result.get('is_found'):
-            return format_return(result.get('path'), depth, True)
+        if show_updates:
+            if len(path) > depth:
+                formatted = format_return(path, depth)
+                print(formatted)
 
-        visited.append(node)
-        queue = result.get('queue')
-    return fn_queue(queue, visited, end, show_updates, depth)
+        depth = len(path)
+        if node not in visited:
+            result = process_node(node, queue, end, path)
+            if result.get('is_found'):
+                return format_return(result.get('path'), depth, True)
 
 
-def run(start, end, show_updates=False):
-    start = start.lower()
-    end = end.lower()
-    start = start.strip()
-    end = end.strip()
+
+        return fn_queue(queue, visited, end, show_updates, depth)
+    return { 'is_found': False }
+
+
+def run(start, end, show_updates=True):
+    start = get_id(start)
+    end = get_id(end)
 
     visited = []
+    # queue = deque([[start]])
     queue = [[start]]
 
-    while queue:
-        return fn_queue(queue, visited, end, show_updates)
+    return fn_queue(queue, visited, end, show_updates)
 
-    return {'is_found': False}
 
 
 if __name__ == "__main__":
+    # ret = get_links('Web Bot')
+    # print(ret)
+    # p = Pool()
     start = sys.argv[1]
     end = sys.argv[2]
     ret = run(start, end)
+    # print(ret)
     if ret.get('is_found', None):
-        print(ret.get('pretty_path'))
+        print(ret.get('path'))
     else:
         print("Sorry, I couldnt find that path")
+
+
